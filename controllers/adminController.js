c601638df65a0efb1158eafd6cb520327fcc8c43
Gouri Nimalasiri <134169188+gourinimalasiri@users.noreply.github.com>
2025-03-10 07:30:08 +0000

    Get all events (admin only)

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -10,3 +10,12 @@ export const getAllUsers = async (req, res) => {
       res.status(500).json({ message: 'Something went wrong' });
     }
   };
+
+  export const getAllEvents = async (req, res) => {
+    try {
+      const events = await Event.find().populate('organizer', 'name email');
+      res.status(200).json(events);
+    } catch (error) {
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  };
